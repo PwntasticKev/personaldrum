e7142ccd986ca04a72092ec1819ddced2d8a5dca
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-05-05 13:27:00 -0600

    IT WORKS!!!!!!! ARDUINO WORKSSSSSSSSS OMGGGGG

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ Board.on('ready', () => {
   const AWS = require('aws-sdk')
   const S3 = require('./s3')
 //aws
-   sensor = new five.Sensor('A0'),
+   sensor = new five.Sensor({
+    pin: "A0", 
+    freq: 30, 
+    threshold: 1
+  })
    express = require('express'),
    http = require('http'),
    server = http.createServer(),
@@ -130,8 +134,9 @@ io.on("connection", (socket) => {
   let recentHits =[]; 
   let start = false;
   let hit = []
-
-const throttle = _.throttle(() => {
+  
+  sensor.on("change", () => {
+// const throttle = _.throttle(() => {
   let val = sensor.scaleTo(1, 40)
   console.log('val', val)
   if(!start){
@@ -140,162 +145,25 @@ const throttle = _.throttle(() => {
 if (val) {
   // recentHits.push(Date.now() - start) // this is pushing the time when the drum was last hit. 
   hit.push(Date.now() - start) // this is pushing the time when the drum was last hit. 
-
-    let filteredHit = hit.filter(filteredHits => {
-if (hit.length !== 1) {
-  let first = hit.unshift()
-     recentHits.push(first)
-     hit = []
- }
-return true
-
+  
+}
+let filteredHit = hit.filter(filteredHits => {
+  if (hit.length !== 1) {
+    let first = hit.shift(hit)
+    recentHits.push(first)
+    hit = []
+    // console.log('first', first)
+    // console.log('this is the hit',hit);
+  }
+  socket.emit("hit", recentHits)
+  return false
+  
 })
-              
-     socket.emit("hit", recentHits)
-      console.log('this is the hit',hit);
-      console.log('this is the recent hit',recentHits);
-        
-      }
-    }, 15, { 'edge': 'leading' })
+console.log('this is the recent hit',recentHits);
+    // }, 15, { 'edge': 'leading' })
 
-  sensor.on("change", throttle)
+
+  })
   })
   
 })
-
-
-  
-  
-  // let io = socket(server)
-  //ideas tips
-  //start recording stop recording
-  // empty array
-  
-  //sockets update live!
-  
- 
-    //every four seconds is an array
-    // if the drum hasnt been hit in 7 seconds. create new array and setdate to 0
-  //   let start = false;
-    
-  //   const throttle = _.throttle(() => {
-  //     let val = sensor.scaleTo(1,100)
-  //     let hit = []
-  //     let valArr= []
-  //     console.log('val = ', val)
-  //           if(!start){
-  //                 start = Date.now();
-  //               }
-  //               // if (val) {
-  //               //   hit.push(start = Date.now())
-  //               //   let filteredHit = hit.filter(filteredHits => {
-  //               //         if (hit.length !== 1) {
-  //               //           let first = hit.unshift()
-  //               //           let remove = hit.splice(2)
-  //               //           console.log('this is the hit array', hit)
-  //               //           console.log('this is first', first)
-  //               //           console.log('this is the removed', remove)
-  //               //           // recentHits.push(hit[0])
-  //               //         }
-  //               //       })
-  //               //       recentHits.push(filteredHit)
-  //               //     }
-                      
-            
-  //                   // recentHits.push(Date.now() - start)
-  //                   socket.emit("hit", recentHits)
-  //                   console.log(recentHits)
-                
-  //             }, 15)
-
-  // sensor.on("change", throttle)
-  //   // thoughts before bed. 
-  //   // will debounce throw off the set intervals?
-  //   // switch starement?
- 
-  // setInterval(() => {
-  //   //if val() {}
-  //   setTimeout(() => {
-  //     console.log('250')
-  //     if (hits == 1 ) {
-  //       hits.push(['hit'])
-  //       console.log('ive been hit!')
-  //     } else {
-  //       hits.push(['rest'])
-  //       console.log('rest baby!')
-  //     }
-  //     // Check beat count and do something
-  //   }, 250)
-  //   setTimeout(() => {
-  //     console.log('500')
-  //   }, 500)
-  //   setTimeout(() => {
-  //     console.log('750')
-  //     // Check beat count and do something
-  //   }, 750)
-  //   setTimeout(() => {
-  //     console.log('1000')
-  //     // Check beat count and do something
-  //   }, 1000)
-  //   setTimeout(() => {
-  //     // Check beat count and do something
-  //   }, 1250)
-  //   setTimeout(() => {
-  //   console.log('1500') 
-  //   // Check beat count and do something
-  //   }, 1500)
-  //   setTimeout(() => {
-  //     console.log('1750')
-  //     // Check beat count and do something
-  //   }, 1750)
-  //   setTimeout(() => {
-  //     console.log('2000')
-  //     // Check beat count and do something
-  //   }, 2000)
-  //   setTimeout(() => {
-  //     console.log('2250')
-  //     // Check beat count and do something
-  //   }, 2250)
-  //   setTimeout(() => {
-  //     console.log('2500')
-  //     // Check beat count and do something
-  //   }, 2500)
-  //   setTimeout(() => {
-  //     console.log('2750')
-  //     // Check beat count and do something
-  //   }, 2750)
-  //   setTimeout(() => {
-  //     console.log('3000')
-  //     // Check beat count and do something
-  //   }, 3000)
-  //   setTimeout(() => {
-  //     console.log('3250')
-  //     // Check beat count and do something
-  //   }, 3250)
-  //   setTimeout(() => {
-  //     console.log('3500')
-  //     // Check beat count and do something
-  //   }, 3500)
-  //   setTimeout(() => {
-  //   console.log('3750') 
-  //   // Check beat count and do something
-  //   }, 3750)
-  //   setTimeout(() => {
-  //     console.log('4000')
-  //     //socket emit?
-  //     // Check beat count and do something
-  //   }, 3980)
-  // }, 4000 /* the time every beat should take */)
-  
-
-  
-  
-  
-  // let io = socket(server)
-  //ideas tips
-  //start recording stop recording
-  // empty array
-  
-  //sockets update live!
-  
-  
